c2cf4409bb6396208428ecc46f3447fd3b5434e7
matthew weintrub <heytrub@gmail.com>
2015-10-02 20:14:14 -0500

    create app, delete files

---
+++ VimeoStaffPicksApp/VimeoStaffPicks/VimeoStaffPicks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VimeoStaffPicks
+//
+//  Created by Matthew Weintrub on 10/2/15.
+//  Copyright (c) 2015 matthew weintrub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
