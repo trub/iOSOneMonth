e52303a7e942b5d55ae2bd139fbf89cf09336ada
matthew weintrub <heytrub@gmail.com>
2015-10-02 21:01:17 -0500

    display array in tableCellView

--- VimeoStaffPicksApp/VimeoStaffPicks/VimeoStaffPicks/StaffPicksViewController.swift
+++ VimeoStaffPicksApp/VimeoStaffPicks/VimeoStaffPicks/StaffPicksViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class StaffPicksViewController: UIViewController {
+class StaffPicksViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView?
+    
+    let items = ["alfie","lee","mattan","syd","zach"]
+    
+    //mark: View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Do any additional setup after loading the view.
-            }
+        
+        self.setUpTableView()
+    }
 
+    // mark: SETUP
+    
+    func setUpTableView() {
+        
+        self.tableView?.registerClass(UITableViewCell.self, forCellReuseIdentifier: "placeholder")
+        
+    }
+    
+    // mark: UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("placeholder") as! UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+
+        
+        return cell
+    }
     
 }
